9a7f299b3d9df558ebca2d0aefbd73512633105c
Dmitri Naumov <dmitri.naumov@ufz.de>
2012-12-12 21:40:00 +0100

    Add simple R-plotting routines.
    This uses amazing ggplot2 R-package.

--- results/processResults.R
+++ results/processResults.R
@@ -24,3 +24,44 @@ createTable = function (pathname)
 		recursive=TRUE)
 	rbind.fill(lapply(files, readBenchmark))
 }
+
+# For duplicate entries except the mflops column select only those with maximum
+# mflops.
+selectMaxMflops = function (df)
+{
+    ddply(df, c(names(df)[names(df)!="mflops"]),
+            function(x)
+            {
+                x[which.max(x$mflops),]
+            }
+         )
+}
+
+# Plot all benchmarks using log-x and log-y scales.
+plotAllBenchmarks = function (bs)
+{
+    p = ggplot(bs, aes(x=size, y=mflops, color=libname)) + facet_grid(arch ~ benchmark)
+    p + geom_line(aes(linetype=libname)) + scale_x_log10() + scale_y_log10()
+}
+
+# Plot a subset of benchmarks using log-x and log-y scales.
+plotBenchmarks = function (bs)
+{
+    # Select a subset of benchmarks and libraries.
+    b = bs[bs$benchmark %in% c("aat","atv","matrix_matrix","matrix_vector") &
+           !bs$libname %in% c("eigen2","blitz"),]
+    p = ggplot(b, aes(x=size, y=mflops, color=libname)) + facet_grid(arch ~ benchmark)
+    p + geom_line() + scale_x_log10() + scale_y_log10()
+}
+
+# Plot a subset of benchmarks using linear x and y scales constraining size to
+# less or equeal to 100.
+plotSmallSizeBenchmarks = function (bs)
+{
+    # Select a subset of benchmarks and libraries.
+    b = bs[bs$benchmark %in% c("aat","atv","matrix_matrix","matrix_vector") &
+           !bs$libname %in% c("eigen2","blitz") &
+           bs$size <= 100,]
+    p = ggplot(b, aes(x=size, y=mflops, color=libname)) + facet_grid(arch ~ benchmark)
+    p + geom_line()
+}
